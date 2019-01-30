bf4358912ddc8370503248fbc48c0e9eb63e0145
MaxAvdeenko <avdmaxoleg@gmail.com>
2019-01-30 18:49:25 +0300

    Classwork lesson4

--- src/test/java/lesson4/SelenideSimpleWithPO.java
+++ src/test/java/lesson4/SelenideSimpleWithPO.java
@@ -10,7 +10,7 @@ import org.testng.annotations.Test;
 
 import static com.codeborne.selenide.Selenide.*;
 
-public class SelenodeSimpleWithPO {
+public class SelenideSimpleWithPO {
 
     private SelenideIndexPage indexPage;
 
