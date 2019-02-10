265dc1ff6d7d9d9a4d91a2f604e50fb75db778a4
MaxAvdeenko <avdmaxoleg@gmail.com>
2019-02-10 13:31:18 +0300

    hw4

--- src/main/java/homeworks/hw4/ex1/enumsWithDiffElementsData/CheckboxSelectors.java
+++ src/main/java/homeworks/hw4/ex1/enumsWithDiffElementsData/CheckboxSelectors.java
@@ -3,7 +3,7 @@ package homeworks.hw4.ex1.enumsWithDiffElementsData;
 public enum CheckboxSelectors {
     // TODO nope, you should not put locators here, it should be in PO only !
     // TODO Names will be better without _SELECTOR
-    //I changed the decision logic and this enum not used now, same story with ColorsSelectors and RadiosSelectors
+    //I changed the logic of tests and this enum not used now, same story with ColorsSelectors and RadiosSelectors
     WATER_SELECTOR("label:nth-child(1)"),
     EARTH_SELECTOR("label:nth-child(2)"),
     WIND_SELECTOR("label:nth-child(3)"),
