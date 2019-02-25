218154411373b8984e340d8680220c380e62bd18
MaxAvdeenko <avdmaxoleg@gmail.com>
2019-02-25 20:22:44 +0300

    lesson 3 jdi

--- src/test/java/jdi/lesson1/JdiSimpleExpample.java
+++ src/test/java/jdi/lesson1/JdiSimpleExpample.java
@@ -2,7 +2,9 @@ package jdi.lesson1;
 
 import base.jdi.JdiSite;
 import base.jdi.entities.User;
+import com.epam.jdi.light.actions.ActionHelper;
 import com.epam.jdi.light.driver.WebDriverFactory;
+import com.epam.jdi.light.settings.WebSettings;
 import com.epam.jdi.light.ui.html.PageFactory;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeSuite;
@@ -14,6 +16,17 @@ public class JdiSimpleExpample {
     @BeforeSuite
     public void beforeSuite() {
         PageFactory.initElements(JdiSite.class);
+        WebSettings.SEARCH_CONDITION = e ->{
+            return e.isDisplayed();
+        };
+
+        ActionHelper.BEFORE_JDI_ACTION = jp -> {
+
+        };
+        ActionHelper.AFTER_JDI_ACTION = (jp, o) -> {
+            return ActionHelper.AFTER_STEP_ACTION.execute(jp, o);
+        };
+
     }
 
     @Test
