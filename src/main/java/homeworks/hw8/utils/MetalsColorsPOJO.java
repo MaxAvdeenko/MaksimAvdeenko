c205cfeaefa03dd7d3dee54580599aac4a6e0de3
MaxAvdeenko <avdmaxoleg@gmail.com>
2019-03-12 10:27:10 +0300

    hw8 fix

---
+++ src/main/java/homeworks/hw8/utils/MetalsColorsPOJO.java
@@ -0,0 +1,19 @@
+package homeworks.hw8.utils;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
+public class MetalsColorsPOJO {
+    public Integer[] summary;
+    public String[] elements;
+    public String color;
+    public String metals;
+    public String[] vegetables;
+
+}
